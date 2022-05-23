cf9d55cc47c4a3d5fcb9db1f9db39f3ca771b658
PhilRandWu <3378814052@qq.com>
2022-05-23 10:49:59 +0800

    倒计时组件

--- src/index.js
+++ src/index.js
@@ -3,21 +3,14 @@
  * @Author: PhilRandWu
  * @Github: https://github/PhilRandWu
  * @Date: 2022-05-22 18:12:14
- * @LastEditTime: 2022-05-23 09:59:47
+ * @LastEditTime: 2022-05-23 10:44:47
  * @LastEditors: PhilRandWu
  */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { StudentList } from './components/StudentList';
-async function fetchData(url) {
-  const data = await fetch(url).then(resp => resp.json()).then(resp => resp.data);
-  return data;
-}
 
-async function render() {
-  const stus = await fetchData('https://console-mock.apipost.cn/app/mock/project/09d61def-199c-4270-829d-f599bcb0591b/app/mock/project/09d61def-199c-4270-829d-f599bcb0591b/app/mock/project/09d61def-199c-4270-829d-f599bcb0591b/app/mock/project/09d61def-199c-4270-829d-f599bcb0591b/app/mock/project/09d61def-199c-4270-829d-f599bcb0591b/');
-  root.render(<StudentList stus={stus} />)
-}
+import Tick from './components/Tick';
 
-render();
 const root = ReactDOM.createRoot(document.getElementById('app'));
+
+root.render(<Tick num={10}/>)
