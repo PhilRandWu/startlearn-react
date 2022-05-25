58f8b3dd029ebbd74afcd458dfdf8631aabcd896
PhilRandWu <3378814052@qq.com>
2022-05-25 19:28:35 +0800

    三栏布局完成

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,40 +1,16 @@
-/*
- * @Description: 
- * @Author: PhilRandWu
- * @Github: https://github/PhilRandWu
- * @Date: 2022-05-25 18:47:53
- * @LastEditTime: 2022-05-25 19:12:51
- * @LastEditors: PhilRandWu
- */
-import React, { Component } from 'react'
-import Modal from '../Modal/Modal'
+import React from 'react'
+import Layout from '../Layout/Layout'
 
-export default class App extends Component {
-    state = {
-        bg: 'rgba(1,1,1,.1)',
-        isLoading: true
+export default function App() {
+    const configProps = {
+        leftWidth: 20,
+        rightWidth: 60,
+        leftContent: <div>左边区域</div>,
+        rightContent: <div>右边区域</div>
     }
-    render() {
-        return (
-            <>
-                <img alt='' src='https://img0.baidu.com/it/u=1858651154,4144960213&fm=253&fmt=auto&app=120&f=JPEG?w=1340&h=800'></img>
-                {
-                    this.state.isLoading ? (
-                        <Modal {...this.state}>加载中...
-                            <button onClick={() => {
-                                this.setState({
-                                    isLoading: false
-                                })
-                            }}>关闭蒙层</button>
-                        </Modal>
-                    ) : null
-                }
-                <button onClick={() => {
-                    this.setState({
-                        isLoading: true
-                    })
-                }}>展示蒙层</button> 
-            </>
-        )
-    }
+    return (
+        <Layout {...configProps}>
+            <h1>中间区域</h1>
+        </Layout>
+    )
 }
