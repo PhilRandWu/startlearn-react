992eaaa84482a6ffd1cb7d0aaa9b90687fa84815
PhilRandWu <3378814052@qq.com>
2022-06-08 16:22:22 +0800

    render props

---
+++ src/components/common/ListenMouse/ShowMousePointer.js
@@ -0,0 +1,19 @@
+/*
+ * @Description: 
+ * @Author: PhilRandWu
+ * @Github: https://github/PhilRandWu
+ * @Date: 2022-06-08 15:58:19
+ * @LastEditTime: 2022-06-08 16:11:53
+ * @LastEditors: PhilRandWu
+ */
+import React from 'react'
+import './style.css'
+
+
+export default function ShowMousePointer() {
+    return (
+        <div className='pointer' style={{
+            
+        }}></div>
+    )
+}
