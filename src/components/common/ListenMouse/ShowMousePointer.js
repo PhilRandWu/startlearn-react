31bf91c7d91595c377998ec71ea6d6654a90da97
PhilRandWu <3378814052@qq.com>
2022-06-08 16:32:20 +0800

    实现案例

--- src/components/common/ListenMouse/ShowMousePointer.js
+++ src/components/common/ListenMouse/ShowMousePointer.js
@@ -3,17 +3,22 @@
  * @Author: PhilRandWu
  * @Github: https://github/PhilRandWu
  * @Date: 2022-06-08 15:58:19
- * @LastEditTime: 2022-06-08 16:11:53
+ * @LastEditTime: 2022-06-08 16:29:27
  * @LastEditors: PhilRandWu
  */
 import React from 'react'
+import withListenMouseMoves from './withListenMouseMoves'
 import './style.css'
 
 
-export default function ShowMousePointer() {
+function ShowMousePointer(props) {
     return (
         <div className='pointer' style={{
-            
+            left: props.mouse.x - 25,
+            top: props.mouse.y -25
         }}></div>
     )
 }
+
+
+export default withListenMouseMoves(ShowMousePointer);
--- src/components/common/ListenMouse/Test.js
+++ src/components/common/ListenMouse/Test.js
@@ -1,26 +1,12 @@
 import React, { PureComponent } from 'react'
-import ListenMouseMoves from './ListenMouseMoves'
+import ShowMousePointer from './ShowMousePointer'
 
 
-const pointer = mouse => (
-  <div className='pointer' style={{
-    left: mouse.x - 25,
-    top: mouse.y - 25
-  }}></div>
-)
-
-const tips = mouse => (
-  <>
-    <div>x:{mouse.x}</div>
-    <div>y:{mouse.y}</div>
-  </>
-)
 export default class Test extends PureComponent {
   render() {
     return (
       <>
-        <ListenMouseMoves render={pointer} />
-        <ListenMouseMoves render={tips} />
+        <ShowMousePointer />
       </>
     )
   }
